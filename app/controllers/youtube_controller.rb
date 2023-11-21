199f98c416f1daa4d658388fcc29956f8b298084
LoganH-R <146144771+LoganH-R@users.noreply.github.com>
2023-11-21 02:14:03 +0000

    returns list of subscribed channels usernames

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -15,8 +15,21 @@ class YoutubeController < ApplicationController
       
       youtube = Google::Apis::YoutubeV3::YouTubeService.new
       youtube.authorization = credentials
-      channel = youtube.list_channels('snippet', mine: true)
-      render :html => "<pre>#{JSON.pretty_generate(channel.to_h)}</pre>".html_safe
+      channels = youtube.list_subscriptions('snippet', mine: true)
+      subscribed_channels = channels.items.map{ |subscription| subscription.snippet.resource_id.channel_id }
+
+      subscribed_channels_usernames = Array.new
+
+      subscribed_channels.each do |channel_id|
+        response = youtube.list_channels('snippet', id: channel_id)
+        channel = response.items.first
+        username = channel.snippet.title
+        subscribed_channels_usernames.push(username)
+      end
+
+      
+
+      render :html => "<pre>#{JSON.pretty_generate(subscribed_channels_usernames.to_h)}</pre>".html_safe
     end
   end
 
